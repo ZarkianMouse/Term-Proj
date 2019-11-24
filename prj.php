de297bd124cb51aa3b2329e7ecbddcfc7b35b2e1
Naomi Burhoe <Naomi.burhoe@hotmail.com>
2019-11-24 12:53:26 -0500

    Update prj.php

--- prj.php
+++ prj.php
@@ -20,15 +20,16 @@ if (mysqli_connect_errno($conn)) {
 die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
-//Create an Insert prepared statement and run it
-$username = 'BrandNewProduct';
-$passwd = 'Blue';
-$score = 15;
-if ($stmt = mysqli_prepare($conn, "INSERT INTO User (Username, Passwd, Score) VALUES (?, ?, ?)")) {
-mysqli_stmt_bind_param($stmt, 'ssd', $username, $passwd, $score);
-mysqli_stmt_execute($stmt);
-printf("Insert: Affected %d rows\n", mysqli_stmt_affected_rows($stmt));
-mysqli_stmt_close($stmt);
+// Run the create table query
+if (mysqli_query($conn, '
+CREATE TABLE Users (
+`Username` VARCHAR(16) NOT NULL ,
+`Passwd` VARCHAR(16) NOT NULL ,
+`Score` Int NOT NULL ,
+PRIMARY KEY (`Username`)
+);
+')) {
+printf("Table created\n");
 }
 
 //Create an Insert prepared statement and run it
