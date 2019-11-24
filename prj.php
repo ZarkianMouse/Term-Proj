92f7c44dcc8ca7d10e570ded540d04c0eb45a738
Naomi Burhoe <Naomi.burhoe@hotmail.com>
2019-11-24 11:03:52 -0500

    updating files

--- prj.php
+++ prj.php
@@ -7,20 +7,34 @@
 
 <body>
 <?php
-// PHP Data Objects(PDO) Sample Code:
-try {
-	$conn = new PDO("sqlsrv:server = tcp:nrbserver.database.windows.net,1433; Database = MyTermProject", "nrb74", "F0rT4E1stTIM3");
-	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-	print("Error connecting to SQL Server.");
-	die(print_r($e));
+$host = 'nrb-term.mysql.database.azure.com';
+$username = 'nrbadmin@nrb-term';
+$password = 'M4W1srtA0l9';
+$db_name = 'nrb-term';
+
+//Establishes the connection
+$conn = mysqli_init();
+mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL) ; 
+mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
+if (mysqli_connect_errno($conn)) {
+die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "nrb74", "pwd" => "F0rT4E1stTIM3", "Database" => "MyTermProject", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:nrbserver.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
+// Run the create table query
+if (mysqli_query($conn, '
+CREATE TABLE Product`(
+`Id` INT NOT NULL AUTO_INCREMENT ,
+`ProductName` VARCHAR(200) NOT NULL ,
+`Color` VARCHAR(50) NOT NULL ,
+`Price` DOUBLE NOT NULL ,
+PRIMARY KEY (`Id`)
+);
+')) {
+printf("Table created\n");
+}
+
+//Close the connection
+mysqli_close($conn);
 ?>
 </body>
 </html>
