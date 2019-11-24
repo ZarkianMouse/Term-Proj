cf2e1a119264da2fd17c3c4902b1b921f9e7b7f9
Naomi Burhoe <Naomi.burhoe@hotmail.com>
2019-11-24 11:17:17 -0500

    Update prj.php

--- prj.php
+++ prj.php
@@ -14,7 +14,7 @@ $db_name = 'nrb-term';
 
 //Establishes the connection
 $conn = mysqli_init();
-mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL) ; 
+//mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL) ; 
 mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
 if (mysqli_connect_errno($conn)) {
 die('Failed to connect to MySQL: '.mysqli_connect_error());
