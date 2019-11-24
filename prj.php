5ae2a7d0458d81084ba1bebffd48de2afe46677a
Naomi Burhoe <Naomi.burhoe@hotmail.com>
2019-11-24 13:12:30 -0500

    Update prj.php

--- prj.php
+++ prj.php
@@ -7,8 +7,8 @@
 
 <body>
 <?php
-$myuser = $_POST["id"];
-$mypass = $_POST["type"];
+$myuser = $_POST["username"];
+$mypass = $_POST["password"];
 $host = 'nrb-term.mysql.database.azure.com';
 $username = 'nrbadmin@nrb-term';
 $password = 'M4W1srtA0l9';
@@ -22,16 +22,6 @@ if (mysqli_connect_errno($conn)) {
 die('Failed to connect to MySQL: '.mysqli_connect_error());
 }	
 
-//Create an Insert prepared statement and run it
-$product_name = 'BrandNewProduct';
-$product_color = 'Red';
-$product_price = 15.5;
-if ($stmt = mysqli_prepare($conn, "INSERT INTO Products (ProductName, Color, Price) VALUES (?, ?, ?)")) {
-mysqli_stmt_bind_param($stmt, 'ssd', $product_name, $product_color, $product_price);
-mysqli_stmt_execute($stmt);
-printf("Insert: Affected %d rows\n", mysqli_stmt_affected_rows($stmt));
-mysqli_stmt_close($stmt);
-}
 
 //Create an Insert prepared statement and run it
 $score = 0;
@@ -39,15 +29,10 @@ if ($stmt = mysqli_prepare($conn, "INSERT INTO Users (Username,Passwd, Score) VA
 mysqli_stmt_bind_param($stmt, 'ssd', $myuser, $mypass, $score);
 mysqli_stmt_execute($stmt);
 printf("Insert: Affected %d rows\n", mysqli_stmt_affected_rows($stmt));
+printf("\n");
 mysqli_stmt_close($stmt);
 }	
 
-//Run the Select query
-printf("Reading data from table: \n");
-$res = mysqli_query($conn, 'SELECT * FROM Products');
-while ($row = mysqli_fetch_assoc($res)) {
-var_dump($row);
-}
 	
 //Run the Select query
 printf("Reading data from table: \n");
