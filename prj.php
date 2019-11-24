d5d0d133d19f8665886df541cfa5d55d164df9fa
Naomi Burhoe <Naomi.burhoe@hotmail.com>
2019-11-24 13:09:16 -0500

    Update prj.php

--- prj.php
+++ prj.php
@@ -7,6 +7,8 @@
 
 <body>
 <?php
+$myuser = $_POST["id"];
+$mypass = $_POST["type"];
 $host = 'nrb-term.mysql.database.azure.com';
 $username = 'nrbadmin@nrb-term';
 $password = 'M4W1srtA0l9';
@@ -32,11 +34,9 @@ mysqli_stmt_close($stmt);
 }
 
 //Create an Insert prepared statement and run it
-$username = 'NewUser';
-$passwd = 'Blue';
 $score = 0;
 if ($stmt = mysqli_prepare($conn, "INSERT INTO Users (Username,Passwd, Score) VALUES (?, ?, ?)")) {
-mysqli_stmt_bind_param($stmt, 'ssd', $username, $passwd, $score);
+mysqli_stmt_bind_param($stmt, 'ssd', $myuser, $mypass, $score);
 mysqli_stmt_execute($stmt);
 printf("Insert: Affected %d rows\n", mysqli_stmt_affected_rows($stmt));
 mysqli_stmt_close($stmt);
@@ -56,8 +56,49 @@ while ($row = mysqli_fetch_assoc($res)) {
 var_dump($row);
 }
 
+if (!$res) {
+    print "Error - the query could not be executed";
+    $error = mysqli_error();
+    print "<p>" . $error . "</p>";
+    exit;
+}	
+
+// Get the number of rows in the result, as well as the first row
+//  and the number of fields in the rows
+$num_rows = mysqli_num_rows($res);
+//print "Number of rows = $num_rows <br />";
+
+print "<table><caption> <h2> MyUsers($num_rows) </h2> </caption>";
+print "<tr align = 'center'>";
+
+$row = mysqli_fetch_array($res);
+$num_fields = mysqli_num_fields($res);
+
+// Produce the column labels
+$keys = array_keys($row);
+for ($index = 0; $index < $num_fields; $index++) 
+    print "<th>" . $keys[2 * $index + 1] . "</th>";
+print "</tr>";
+    
+// Output the values of the fields in the rows
+for ($row_num = 0; $row_num < $num_rows; $row_num++) {
+    print "<tr align = 'center'>";
+    $values = array_values($row);
+    for ($index = 0; $index < $num_fields; $index++){
+        $value = htmlspecialchars($values[2 * $index + 1]);
+        print "<th>" . $value . "</th> ";
+    }
+    print "</tr>";
+    $row = mysqli_fetch_array($res);
+}
+print "</table>";	
+	
+	
 //Close the connection
 mysqli_close($conn);
 ?>
+	
+		
+
 </body>
 </html>
