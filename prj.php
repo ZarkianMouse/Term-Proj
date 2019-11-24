9218fa8a8243c87573068c2ace964d904b0087a6
Naomi Burhoe <Naomi.burhoe@hotmail.com>
2019-11-24 15:05:04 -0500

    Update prj.php

--- prj.php
+++ prj.php
@@ -20,31 +20,34 @@ if ($myuser == "" || $mypass == "")
 	echo "Cannot proceed with query\n";
 else
 {
-//Establishes the connection
-$conn = mysqli_init();
-mysqli_ssl_set($conn,NULL,NULL, "BaltimoreCyberTrustRoot.crt.pem", NULL, NULL) ; 
-mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, MYSQLI_CLIENT_SSL);
-if (mysqli_connect_errno($conn)) {
-die('Failed to connect to MySQL: '.mysqli_connect_error());
-}	
-    
+	//Establishes the connection
+	$conn = mysqli_init();
+	mysqli_ssl_set($conn,NULL,NULL, "BaltimoreCyberTrustRoot.crt.pem", NULL, NULL) ; 
+	mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, MYSQLI_CLIENT_SSL);
+	if (mysqli_connect_errno($conn)) {
+		die('Failed to connect to MySQL: '.mysqli_connect_error());
+	}	
+	
+    $score = 0;
+	if($action == "insert")
+	{
+		$query = "insert into Users values($myuser,$mypass, $score)";
+		if ($stmt = mysqli_prepare($conn, "INSERT INTO Users (Username,Passwd, Score) VALUES (?, ?, ?)")) {
+			mysqli_stmt_bind_param($stmt, 'ssd', $myuser, $mypass, $score);
+			mysqli_stmt_execute($stmt);
+			printf("\n");
+			mysqli_stmt_close($stmt);
+		}	
+	}
+	else
+	{
+		$query = "SELECT * FROM Users WHERE Username";
+	}
 
-// print "<b> The action is: </b> $action <br />";
 
-$score = 0;
 
 if($action == "display")
     $query = "";
-else if ($action == "insert")
-{
-    $query = "insert into Users values($myuser,$mypass, $score)";
-	if ($stmt = mysqli_prepare($conn, "INSERT INTO Users (Username,Passwd, Score) VALUES (?, ?, ?)")) {
-		mysqli_stmt_bind_param($stmt, 'ssd', $myuser, $mypass, $score);
-		mysqli_stmt_execute($stmt);
-		printf("\n");
-		mysqli_stmt_close($stmt);
-	}	
-}
 else if ($action == "update")
 {
 	//Run the Update statement
@@ -83,7 +86,7 @@ else if ($action == "delete")
 //}
     
 // Final Display of All Entries
-$query = "SELECT * FROM Users";
+$query = "SELECT Username, Score FROM Users";
 $result = mysqli_query($conn,$query);
 
 // Get the number of rows in the result, as well as the first row
@@ -101,7 +104,6 @@ $num_fields = mysqli_num_fields($result);
 $keys = array_keys($row);
 for ($index = 0; $index < $num_fields; $index++)
 {
-	if ($index != 1)
 		print "<th>" . $keys[2 * $index + 1] . "</th>";
 }
     
@@ -112,11 +114,8 @@ for ($row_num = 0; $row_num < $num_rows; $row_num++) {
     print "<tr align = 'center'>";
     $values = array_values($row);
     for ($index = 0; $index < $num_fields; $index++){
-		if ($index != 1)
-		{
 			$value = htmlspecialchars($values[2 * $index + 1]);
 			print "<th>" . $value . "</th> ";
-		}
     }
     print "</tr>";
     $row = mysqli_fetch_array($result);
